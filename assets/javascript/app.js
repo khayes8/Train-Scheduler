f92178009ea39d6ee9966b2f71406796f29ad9a0
Kelsey Hayes <kelseyhayes@Kelseys-MacBook-Pro.local>
2017-07-29 15:04:15 -0400

    data  stored won click submit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,27 +20,51 @@ var minutesAway = 0;
 
 // });
 
-$(".submit").on("click", function(storeInput){
-  event.preventDefault();
-  // console.log("hi");
-  trainTime = $("#trainInput").val().trim();
-  destination = $("#destinationInput").val().trim();
-  frequency = $("#frequencyInput").val().trim();
-  nextArrival = $("#nextarrivalInput").val().trim();
-  minutesAway = $("#minutesawayInput").val().trim();
+$(".submit").on("click", function(){
+event.preventDefault();
+// console.log("hi");
+trainTime = $("#trainInput").val().trim();
+destination = $("#destinationInput").val().trim();
+frequency = $("#frequencyInput").val().trim();
+nextArrival = $("#nextarrivalInput").val().trim();
+minutesAway = $("#minutesawayInput").val().trim();
 
 
-  console.log(trainTime);
-  console.log(destination);
-  console.log(frequency);
-  console.log(nextArrival);
-  console.log(minutesAway);
+console.log(trainTime);
+console.log(destination);
+console.log(frequency);
+console.log(nextArrival);
+console.log(minutesAway);
 
-  database.ref().push({
-  traintime: trainTime,
-  destination: destination,
-  frequency: frequency,
-  nextarrival: nextArrival,
-  minutesaway: minutesAway,
-  dateAdded: firebase.database.ServerValue.TIMESTAMP
+database.ref().push({
+traintime: trainTime,
+destination: destination,
+frequency: frequency,
+nextarrival: nextArrival,
+minutesaway: minutesAway,
+dateAdded: firebase.database.ServerValue.TIMESTAMP
 });
+
+});
+
+
+
+database.ref().on(“child_added”, function(childSnapshot){
+    
+// Log everything that’s coming out of snapshot
+
+
+console.log(childSnapshot.val().trainTime);
+console.log(childSnapshot.val().destination);
+console.log(childSnapshot.val().frequency);
+console.log(childSnapshot.val().nextArrival);
+console.log(childSnapshot.val().minutesAway);
+
+// $(“#full-member-list”).append(“<div class=‘well’><span id= ‘name’>”
+
+// childSnapshot.val().name +
+// “</span><span id=‘email’> ” +  childSnapshot.val().email +
+// “</span><span id=‘age’> ” + childSnapshot.val().age +
+// “</span><span id=‘comment’> ” + childSnapshot.val().comment +“</span></div>“;
+}
+
